778ce22122c19119a5da5cbb780b02947d55412d
Boaz <boazanicet@ymail.com>
2024-06-28 20:27:58 -0400

    Form Components

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+
+interface FormSubmitProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?: "default" | "destructive" | "outline" | "primary" | "secondary" | "ghost" | "link";
+}
+
+export const FormSubmit = ({ children, disabled, className, variant }: FormSubmitProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      className={cn(className)}
+      disabled={pending || disabled}
+      variant={variant}
+      size="sm"
+    >
+      {children}
+    </Button>
+  );
+};
